b6496efc4429209101134d0d1d3e6b4a1d339b91
KevinBett <kevinbett458@gmail.com>
2023-04-27 13:13:38 +0300

    added more details

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     <div>
       <h1>About Us</h1>
       <p>We are from Bravo class in Moringa</p>
-      <p>Contact us to learn more</p>
+      <p>Contact us to learn more about us</p>
+      <p>email us @ bravo@moringaschool.com</p>
     </div>
   )
 }
